f3cc4187bdb4fa368cab07b5312119205021b809
jonassbarai <jonas_sbarai@hotmail.com>
2022-03-06 15:09:56 -0300

    classes criadas

---
+++ src/Application/Program.java
@@ -0,0 +1,16 @@
+package Application;
+
+import model.Entities.Department;
+
+public class Program {
+	
+	public static void main(String [] args) {
+	
+	Department obj = new Department(1,"Books");
+	
+	System.out.println(obj);
+	
+	
+	}
+	
+}
